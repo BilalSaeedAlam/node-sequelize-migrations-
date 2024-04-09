0336459f7c6f73abeff8ae5f81633634ce75c453
BilalSaeedAlam <bilal_ahmed@outlook.com>
2024-04-09 12:54:17 +0500

    Modal with extend method

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
-const {  DataTypes } = require('sequelize');
+const {  DataTypes, Model } = require('sequelize');
 const sequelize = require("./index")
 
-const User = sequelize.define('User', {
+class User extends Model {}
+
+User.init({
   // Model attributes are defined here
   firstName: {
     type: DataTypes.STRING,
@@ -13,11 +15,38 @@ const User = sequelize.define('User', {
   }
 }, {
   // Other model options go here
-  tableName:"users",
-//   timestamps:false
+  sequelize, // We need to pass the connection instance
+  modelName: 'User' // We need to choose the model name
 });
 
-// `sequelize.define` also returns the model
+// the defined model is the class itself
 console.log(User === sequelize.models.User); // true
-
 module.exports = User
+
+
+// **
+// Sequelize Define Method
+// **
+// const {  DataTypes } = require('sequelize');
+// const sequelize = require("./index")
+
+// const User = sequelize.define('User', {
+//   // Model attributes are defined here
+//   firstName: {
+//     type: DataTypes.STRING,
+//     allowNull: false
+//   },
+//   lastName: {
+//     type: DataTypes.STRING
+//     // allowNull defaults to true
+//   }
+// }, {
+//   // Other model options go here
+//   tableName:"users",
+// //   timestamps:false
+// });
+
+// // `sequelize.define` also returns the model
+// console.log(User === sequelize.models.User); // true
+
+// module.exports = User
