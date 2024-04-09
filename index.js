e822c367430f75211eb4e433fb626d70a7671d54
BilalSaeedAlam <bilal_ahmed@outlook.com>
2024-04-09 11:41:20 +0500

    connect database with mysql using sequelize

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express()
 
+require("./models")
+
 app.get("/", (req,res)=>{
     console.log("Hello World")
 });
---
+++ models/index.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('node_sequelize', 'root', null, {
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  });
+
+  try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
