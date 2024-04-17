4a88cf2e8d4aaefc612931f6bdbe36daaa30ba4e
BilalSaeedAlam <bilal_ahmed@outlook.com>
2024-04-17 13:44:19 +0500

    create user with create method instead of build

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,12 @@
 var db = require("../models");
 var User = db.User;
+
 var addUser = async (req, res) => {
-  const jane = User.build({ firstName: "Jane" });
+  const jane = await User.create({ firstName: "Bilal", lastName: "Ahmed" });
+  // const jane = User.build({ firstName: "Jane" });
   console.log(jane instanceof User); // true
   console.log(jane.firstName); // "Jane"
-  await jane.save();
+  // await jane.save();
   console.log("Jane was saved to the database!");
   console.log(jane.toJSON());
   res.status(200).json(jane.toJSON());
